704eac24445eb82e57d7ebf0aa2626418c40c01a
anurag-bhujbal <anuragbhujbal13@gmail.com>
2024-11-04 22:20:54 +0530

    main prod

--- lib/utils.ts
+++ lib/utils.ts
@@ -86,12 +86,14 @@ export function removeKeysFromQuery({
 
 // DEBOUNCE
 export const debounce = (func: (...args: any[]) => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout | null;
+  let timeoutId: NodeJS.Timeout | null = null;
+  
   return (...args: any[]) => {
     if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+    timeoutId = setTimeout(() => func(...args), delay); // Use the spread operator here
   };
 };
+
 
 // GE IMAGE SIZE
 export type AspectRatioKey = keyof typeof aspectRatioOptions;
