3096e3d11cb77e4493fc99a5d6afd42c899f4d93
anurag-bhujbal <anuragbhujbal13@gmail.com>
2024-11-04 21:56:41 +0530

    some minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { IBM_Plex_Sans} from "next/font/google"
 import { cn } from "@/lib/utils";
