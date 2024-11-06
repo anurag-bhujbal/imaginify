eecbceb7bdbead52c98a13aa39cefe12bf5dcc93
anurag-bhujbal <anuragbhujbal13@gmail.com>
2024-11-06 11:12:20 +0530

    Prod Commit

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
 
-const AddTransformationTypePage = () => {
+import Header from '@/components/shared/Header'
+import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { auth } from '@clerk/nextjs/server';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+
+type TransformationTypeKeys = keyof typeof transformationTypes;
+
+const AddTransformationTypePage = async ({ params }: { params: { type: TransformationTypeKeys } }) => {
+  const { userId } = await auth();
+  const { type } = await params;
+  const transformation = transformationTypes[type];
+  if(!userId) redirect('/sign-in')
+  const user = await getUserById(userId);
   return (
-    <div>AddTransformationTypePage</div>
-  )
+    <>
+      <Header 
+          title={transformation.title}
+          subtitle={transformation.subTitle}
+      />
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
 }
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
